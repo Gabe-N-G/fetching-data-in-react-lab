ae9e310826235af4733d5220245dd7a69ee2d11b
g g <gabedotgutierrez@gmail.com>
2024-10-16 16:25:39 -0400

    controller made

---
+++ src/components/StarshipList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StarshipList = () => {
+  return (
+    <div>StarshipList</div>
+  )
+}
+
+export default StarshipList
---
+++ src/services/starshipService.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+
+export {index, search}
+
+const BASE_URL = `https://swapi.dev/api/starships`;
+
+const index = async () =>{
+            try {
+                const res = await fetch(BASE_URL)
+                const data = await res.json()
+                console.log('Data', data)
+            } catch (err){
+                console.log(err)
+            }
+        }
+
+const search = async (ship) =>  {
+    try {
+        const queryString = `?search=${ship}`
+        const res = await fetch(BASE_URL + queryString)
+        const data = await res.json()
+        console.log('Data', data)
+    } catch (err){
+        console.log(err)
+    }
+}
+
+index()
